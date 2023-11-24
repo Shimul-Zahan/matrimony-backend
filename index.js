c6e0a1f2b45ea2c0b11b7a9dc0276e25bcc18be1
AlgoWebBot <shimulzahan636@gmail.com>
2023-11-25 05:33:47 +0600

    user edit biodata api create

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+require('dotenv').config();
 
 app.use(cors({
     origin: ['http://localhost:5173'],
@@ -9,9 +10,47 @@ app.use(cors({
 }));
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('I am ready now !!!')
-})
+console.log(process.env.DB_NAME, process.env.DB_PASS)
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@shimulclaster1.85diumq.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+
+        // await client.connect();
+        const allUsers = client.db("matrmonyDB").collection("allUsers");
+
+        app.get('/', async (req, res) => {
+            res.send('Hello i am ready')
+        })
+
+        app.post('/edit-biodata', async (req, res) => {
+            try {
+                const biodata = req.body;
+                const count = await allUsers.estimatedDocumentCount();
+                const result = await allUsers.insertOne({biodataId: count + 1, ...biodata})
+                console.log(result)
+            } catch (error) {
+                console.log(error)
+            }
+        })
+
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+    }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
     console.log(`Server running at localhost: ${port}`)
